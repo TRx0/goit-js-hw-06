a9ddf21b5924c339082ad9e6df9a8dce608c96ce
Ta <tarasragumov28@gmail.com>
2022-07-03 23:15:44 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -12,16 +12,24 @@ const ingredients = [
 // const ingredientsUl = document.getElementById(`ingredients`);
 // ingredientsUl.append(headEl);
 
-const getIngredients = ingredients.map((element) => {
-  const headEl = document.createElement(`li`);
+// const getIngredients = ingredients.map((element) => {
+//   const headEl = document.createElement(`li`);
 
-  const ingredientsUl = document.getElementById(`ingredients`);
-  ingredientsUl.append(headEl);
-  headEl.textContent = element;
-  headEl.classList.add("item")
+//   const ingredientsUl = document.getElementById(`ingredients`);
+//   ingredientsUl.append(headEl);
+//   headEl.textContent = element;
+//   headEl.classList.add("item")
   
-});
+// });
 
+const getIngredients = document.querySelector("#ingredients");
+   const addLi = ingredients.map(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.classList.add("item");
+    return li;
+    });
+    getIngredients.append(...addLi);
 
 
 // console.log(document.getElementById(`ingredients`))
